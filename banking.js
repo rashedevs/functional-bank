b1e245fc65aea2cb6e5880a7f3f3ed23b0998528
rashedevs <rasheduap2015@gmail.com>
2022-02-11 18:59:42 +0600

    balance function added

--- banking.js
+++ banking.js
@@ -7,11 +7,19 @@ function getInputValue(inputID) {
     return inputAmount;
 }
 function updateTotal(totalFieldId, amount) {
-    debugger;
+    // debugger;
     const prevTotal = document.getElementById(totalFieldId);
     const prevTotalText = prevTotal.innerText;
     prevTotal.innerText = parseFloat(prevTotalText) + parseFloat(amount);
     return prevTotalText;
+}
+function updateBalance(reqAmount, isAdd) {
+    const balanceTotal = document.getElementById('balance');
+    const balanceTotalText = balanceTotal.innerText;
+    if (isAdd == true) {
+        balanceTotal.innerText = parseFloat(balanceTotalText) + parseFloat(reqAmount);
+    }
+    else { balanceTotal.innerText = parseFloat(balanceTotalText) - parseFloat(reqAmount); }
 }
 
 //-----------------------------------------------------------------------------------------
@@ -19,22 +27,13 @@ document.getElementById('deposit-baton').addEventListener('click', function () {
     const depositAmount = getInputValue('deposit');
     // update corresponding chart---------------------------
     updateTotal('deposit-total', depositAmount);
-
-    //update account balance-------------------------------------
-    const balanceTotal = document.getElementById('balance');
-    const balanceTotalText = balanceTotal.innerText;
-    balanceTotal.innerText = parseFloat(balanceTotalText) + parseFloat(depositAmount);
-
+    //update account balance--------------------------------
+    updateBalance(depositAmount, true);
 })
 
 document.getElementById('withdraw-baton').addEventListener('click', function () {
     const withdrawAmount = getInputValue('withdraw');
-    // update corresponding chart---------------------------
     updateTotal('withdraw-total', withdrawAmount);
-
-    //update account balance----------------------------------------
-    const balanceTotal = document.getElementById('balance');
-    const balanceTotalText = balanceTotal.innerText;
-    balanceTotal.innerText = parseFloat(balanceTotalText) - parseFloat(withdrawAmount);
+    updateBalance(withdrawAmount, false);
 })
 
